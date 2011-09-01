af0dbdcb0ebfe19438b57a91bef9673e83b88d87
Alasdair Lumsden <al@everycity.co.uk>
2011-09-01 17:31:06 +0100

    Updating colours in shell/standard-settings

--- components/shellsettings/files/bash.d/20_colours.sh
+++ components/shellsettings/files/bash.d/20_colours.sh
@@ -14,21 +14,38 @@
 
 # Set up some attractive looking shell colours
 rgb_restore="\033[00m"
-rgb_black="\033[00;30m"
-rgb_firebrick="\033[00;31m"
-rgb_red="\033[01;31m"
-rgb_forest="\033[00;32m"
-rgb_green="\033[01;32m"
-rgb_brown="\033[00;33m"
-rgb_yellow="\033[01;33m"
-rgb_navy="\033[00;34m"
-rgb_blue="\033[01;34m"
-rgb_purple="\033[00;35m"
-rgb_magenta="\033[01;35m"
-rgb_cadet="\033[00;36m"
-rgb_cyan="\033[01;36m"
-rgb_gray="\033[00;37m"
-rgb_white="\033[01;37m"
 
-rgb_std=$rgb_white
+txtblk='\e[0;30m' # Black - Regular
+txtred='\e[0;31m' # Red
+txtgrn='\e[0;32m' # Green
+txtylw='\e[0;33m' # Yellow
+txtblu='\e[0;34m' # Blue
+txtpur='\e[0;35m' # Purple
+txtcyn='\e[0;36m' # Cyan
+txtwht='\e[0;37m' # White
+bldblk='\e[1;30m' # Black - Bold
+bldred='\e[1;31m' # Red
+bldgrn='\e[1;32m' # Green
+bldylw='\e[1;33m' # Yellow
+bldblu='\e[1;34m' # Blue
+bldpur='\e[1;35m' # Purple
+bldcyn='\e[1;36m' # Cyan
+bldwht='\e[1;37m' # White
+unkblk='\e[4;30m' # Black - Underline
+undred='\e[4;31m' # Red
+undgrn='\e[4;32m' # Green
+undylw='\e[4;33m' # Yellow
+undblu='\e[4;34m' # Blue
+undpur='\e[4;35m' # Purple
+undcyn='\e[4;36m' # Cyan
+undwht='\e[4;37m' # White
+bakblk='\e[40m'   # Black - Background
+bakred='\e[41m'   # Red
+badgrn='\e[42m'   # Green
+bakylw='\e[43m'   # Yellow
+bakblu='\e[44m'   # Blue
+bakpur='\e[45m'   # Purple
+bakcyn='\e[46m'   # Cyan
+bakwht='\e[47m'   # White
+txtrst='\e[0m'    # Text Reset
 
