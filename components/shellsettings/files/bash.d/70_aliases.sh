383a24dda55155e44503e1431b2bbb82a189a11c
Alasdair Lumsden <al@everycity.co.uk>
2011-09-01 17:26:14 +0100

    Adding aliases to shell/standard-settings

---
+++ components/shellsettings/files/bash.d/70_aliases.sh
@@ -0,0 +1,21 @@
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL)". You may
+# only use this file in accordance with the terms of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+#
+
+#
+# Copyright 2011 EveryCity Ltd. All rights reserved.
+#
+
+# Some delightful colours
+alias ls='/ec/bin/ls --color=auto'
+
+# Some helpful aliases for Linux converts
+alias top='prstat -Z'
+alias ll='/ec/bin/ls --color=auto -l'
+alias pp='/usr/bin/ps -ef'
