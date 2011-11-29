327bb4052b45c6fd92e74bfc950a425ca71faae2
Andrzej Szeszo <aszeszo@gmail.com>
2011-11-29 12:12:06 +0000

    Initial cacti package

---
+++ components/webapps/cacti/files/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$database_type = "mysql";
+$database_default = "cacti";
+$database_hostname = "";
+$database_username = "cacti";
+$database_password = "cacti";
+$database_port = "";
+$database_ssl = false;
+
+$path_cactilog = "/ec/var/log/cacti/cacti.log";
+
+?>
+
