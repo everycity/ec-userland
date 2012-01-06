e24d173a467f6fe6a06ede4916fcc43ace9b6a13
Alasdair Lumsden <al@everycity.co.uk>
2012-01-06 17:35:41 +0000

    nrpe package modifications
    --HG--
    rename : components/nrpe/files/nrpe.cfg => components/nrpe/files/nrpe.conf

--- components/nrpe/files/nrpe-startup.sh
+++ components/nrpe/files/nrpe-startup.sh
@@ -1,75 +1,65 @@
-#!/bin/sh
-# DESC: SMF method definitions/wrapper for NRPE.
-# VERSION: $id$
+#!/usr/xpg4/bin/sh
 #
-# Distributed under the BSD License.
-#    
-# Copyright (c) 2007 DigiTar
-# All Rights Reserved
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL)". You may
+# only use this file in accordance with the terms of the CDDL.
 #
-#    Redistribution and use in source and binary forms, with or without modification, 
-#    are permitted provided that the following conditions are met:
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
 #
-#        * Redistributions of source code must retain the above copyright notice,
-#          this list of conditions and the following disclaimer.
-#        * Redistributions in binary form must reproduce the above copyright notice, 
-#          this list of conditions and the following disclaimer in the documentation 
-#          and/or other materials provided with the distribution.
-#        * Neither the name of DigiTar nor the names of its contributors may be
-#          used to endorse or promote products derived from this software without 
-#          specific prior written permission.
+
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
-# SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
-# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
-# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
-# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
-# SUCH DAMAGE.
+# Copyright 2011 EveryCity Ltd. All rights reserved.
 #
 
 . /lib/svc/share/smf_include.sh
 
-PREFIX=/ec
-ETC=${PREFIX}/etc
-BIN=${PREFIX}/bin
-PID_FILE=/var/run/nrpe.pid
+# Do not change these defaults below, they will be overwritten
+# during the next package upgrade. Instead, set these properties
+# via SMF using "svccfg -s nrpe:default" and setprop
 
-nrpe_daemon="$BIN/nrpe -c $ETC/nrpe.cfg -d"
+config_file="/ec/etc/nagios/nrpe.conf"
+nrpe_binary="/ec/bin/nrpe"
+additional_startup_options=""
 
-[ -f "$BIN/nrpe" ] || exit $SMF_EXIT_ERR_FATAL
+getprop() {
+    PROPVAL=""
+    svcprop -q -p $1 ${SMF_FMRI}
+    if [ $? -eq 0 ] ; then
+        PROPVAL=`svccfg -s ${SMF_FMRI} listprop $1 | \
+		/usr/bin/nawk '{ for (i = 3; i <= NF; i++) printf $i" " }' | \
+		/usr/bin/nawk '{ sub(/^\"/,""); sub(/\"[ \t]*$/,""); print }' | \
+		/usr/bin/sed 's/[ ]*$//g'`
+        if [ "${PROPVAL}" = "\"\"" ] ; then
+            PROPVAL=""
+        fi
+        return
+    fi
+    return
+}
 
+varprop() {
+  getprop nrpe/$1
+  if [ "${PROPVAL}" != "" ] ; then
+    export $1="${PROPVAL}"
+  fi
+}
+
+varprop config_file
+varprop additional_startup_options
 
 case "$1" in
-	start)
-        $nrpe_daemon
-        if test "$?" = "0" 
-        then
-            exit $SMF_EXIT_OK
-        elif test "$?" = "2"
-        then
-            echo "$ETC/nrpe.cfg does not exist. NRPE cannot be started without a configuration file."
-            exit $SMF_EXIT_ERR_FATAL
-        else
-            echo "An unexpected error occured while trying to start NRPE."
-            exit $SMF_EXIT_ERR_FATAL
-        fi  
-	;;		
-
-	refresh)
-        kill -HUP `cat $PID_FILE`      # Send a SIGHUP to the NRPE process
-	if test "$?" = "0" 
-        then
-            echo "NRPE configuration succesfully reloaded."
-            exit $SMF_EXIT_OK
-        else
-            echo "An unexpected error occured while instructing NRPE to reload its configuration. Is the NRPE process running?"
-            exit $SMF_EXIT_ERR_FATAL
-        fi
-    ;;		
+  start)
+    echo "Starting nrpe: \c"
+    $nrpe_binary $additional_startup_options -d -c $config_file 
+    echo "nrpe."
+    ;;
+  *)
+    echo "Usage: $0 {start}"
+    exit 1
+    ;;
 esac
 
-exit $SMF_EXIT_OK
+exit 0
+
