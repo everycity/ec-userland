9054d6c6925bc7bcb668116bc74cb8558631d6c4
Alasdair Lumsden <al@everycity.co.uk>
2012-01-26 18:20:19 +0000

    Enhancing mkrepo.sh to support a publisher prefix argument

--- mkrepo.sh
+++ mkrepo.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 MACH=`uname -p`
+[ "x$1" = "x" ] && repo_prefix=s10.pkg.ec || repo_prefix=$1
 
 [ -d $MACH/repo ] && mv $MACH/repo $MACH/repo.`date +%Y-%m-%d-%H:%M:%S`
 [ -d $MACH ] || mkdir $MACH
 
 pkgrepo create $MACH/repo
-pkgrepo set -s $MACH/repo publisher/prefix=s10.pkg.ec
+pkgrepo set -s $MACH/repo publisher/prefix=$repo_prefix
