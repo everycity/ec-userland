71175a3a9180504659d0dc4abcd723f220cc0eba
Alasdair Lumsden <al@everycity.co.uk>
2012-04-25 16:38:31 +0000

    Disabling compression in openssh due to limitations, cosmetic enhancements

--- components/openssh/files/openssh-startup.sh
+++ components/openssh/files/openssh-startup.sh
@@ -1,19 +1,37 @@
 #!/bin/sh
+
 #
-# Copyright (c) EveryCity 2011
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL)". You may
+# only use this file in accordance with the terms of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+#
+
+#
+# Copyright 2012 EveryCity Ltd. All rights reserved.
 #
 
 . /lib/svc/share/smf_include.sh
 
-SSHDBINARY="/ec/bin/sshd"
+PREFIXDIR="/ec"
+SSHDBINARY="${PREFIXDIR}/bin/sshd"
 
 case "$1" in
   start)
-    [ -f /ec/etc/ssh/ssh_host_rsa_key ] || /ec/bin/ssh-keygen -N '' -t rsa -f /ec/etc/ssh/ssh_host_rsa_key
-    [ -f /ec/etc/ssh/ssh_host_dsa_key ] || /ec/bin/ssh-keygen -N '' -t dsa -f /ec/etc/ssh/ssh_host_dsa_key 
-    echo "Starting OpenSSH: \c"
+    # Preparing host ssh keys
+    if [ ! -f ${PREFIXDIR}/etc/ssh/ssh_host_rsa_key ] ; then
+      ${PREFIXDIR}/bin/ssh-keygen -N '' -t rsa -f ${PREFIXDIR}/etc/ssh/ssh_host_rsa_key
+
+      # Only generate the ecdsa key if no rsa key exists to avoid causing known_host warnings when people upgrade openssh
+      [ ! -f ${PREFIXDIR}/etc/ssh/ssh_host_ecdsa_key ] && ${PREFIXDIR}/bin/ssh-keygen -N '' -t ecdsa -f ${PREFIXDIR}/etc/ssh/ssh_host_ecdsa_key
+    fi
+
+    [ -f ${PREFIXDIR}/etc/ssh/ssh_host_dsa_key ] || ${PREFIXDIR}/bin/ssh-keygen -N '' -t dsa -f ${PREFIXDIR}/etc/ssh/ssh_host_dsa_key
+
     $SSHDBINARY
-    echo "sshd."
     ;;
   *)
     echo "Usage: $0 {start}"
