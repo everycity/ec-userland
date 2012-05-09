47bf020475dae696e842aa891ea6a0a0ab8e3f45
Jon Tibble <jt@everycity.co.uk>
2012-05-09 10:39:54 +0000

    Add charybdis

---
+++ components/charybdis/files/charybdis-startup.sh
@@ -0,0 +1,31 @@
+#!/usr/xpg4/bin/sh
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL)". You may
+# only use this file in accordance with the terms of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+#
+
+#
+# Copyright 2011 EveryCity Ltd. All rights reserved.
+#
+
+. /lib/svc/share/smf_include.sh
+
+case "$1" in
+  start)
+    echo "Starting charybdis: \c"
+    /ec/lib/charybdis/ircd 
+    echo "lighttpd."
+    ;;
+  *)
+    echo "Usage: $0 {start}"
+    exit 1
+    ;;
+esac
+
+exit 0
+
