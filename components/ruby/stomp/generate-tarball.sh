83e0a9bf79c341a9bbc0be124dd42a25add341f8
Jon Tibble <jt@everycity.co.uk>
2013-06-06 16:32:53 +0100

    Fix stomp download

--- components/ruby/stomp/generate-tarball.sh
+++ components/ruby/stomp/generate-tarball.sh
@@ -2,11 +2,12 @@
 
 GIT=/ec/bin/git
 
-if [ ! -s $2-$3.tar.gz ]; then
+if [ ! -s $4/$2-$3.tar.gz ]; then
     [ -d $2 ] && rm -rf $2
     $GIT clone $1 $2
     cd $2
     $GIT checkout $3
     cd ..
     tar cf - $2 | gzip -9 >$2-$3.tar.gz
+    mv $2-$3.tar.gz $4/
 fi
