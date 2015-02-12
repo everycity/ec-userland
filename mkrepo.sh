715c530f18d010e31edcf5499431bb4fe012f6b9
Andrzej Szeszo <as@everycity.co.uk>
2015-02-12 13:40:48 +0000

    Some convenience updates

--- mkrepo.sh
+++ mkrepo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 MACH=`uname -p`
-[ "x$1" = "x" ] && repo_prefix=s10.pkg.ec || repo_prefix=$1
+[ "x$1" = "x" ] && repo_prefix=smartos.pkg.ec || repo_prefix=$1
 
 [ -d $MACH/repo ] && mv $MACH/repo $MACH/repo.`date +%Y-%m-%d-%H:%M:%S`
 [ -d $MACH ] || mkdir $MACH
