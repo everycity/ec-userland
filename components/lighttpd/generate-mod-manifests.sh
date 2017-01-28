0a47aac24a42011813fed5456ec7cc3dc0a8f277
Jon Tibble <jt@everycity.co.uk>
2017-01-28 20:00:21 +0000

    Bump lighttpd to 1.4.45

--- components/lighttpd/generate-mod-manifests.sh
+++ components/lighttpd/generate-mod-manifests.sh
@@ -10,10 +10,10 @@
 #
 
 #
-# Copyright 2011 EveryCity Ltd. All rights reserved.
+# Copyright 2017 EveryCity Ltd. All rights reserved.
 #
 
-SAMPLEMANIFEST=sample-manifests/lighttpd-1.4.30
+SAMPLEMANIFEST=build/manifest-i386-generated.p5m
 MODULES=`cat $SAMPLEMANIFEST | grep mod_ | grep -v MACH64 | grep '\.so$' | sed -e 's/.*mod_//g' -e 's/\.so$//g'`
 
 for i in $MODULES ; do
@@ -30,16 +30,15 @@ cat <<EOF > lighttpd-mod-${i}.p5m
 #
 
 #
-# Copyright 2011 EveryCity Ltd. All rights reserved.
+# Copyright $(date +%Y) EveryCity Ltd. All rights reserved.
 #
 
 set name=pkg.fmri value=pkg:/web/server/lighttpd-\$(LIGHTTPD_MAJOR)/module/lighttpd-${i}@\$(IPS_COMPONENT_VERSION),\$(BUILD_VERSION)
 set name=pkg.summary value="${i} lighttpd module"
-set name=info.upstream_url value="http://www.lighttpd.net/"
-set name=info.source_url value=\$(COMPONENT_ARCHIVE_URL)
-set name=org.opensolaris.consolidation value=\$(CONSOLIDATION)
+set name=info.upstream-url value="\$(COMPONENT_PROJECT_URL)"
+set name=info.source-url value=\$(COMPONENT_ARCHIVE_URL)
 
-license lighttpd.license license="Revised BSD"
+license \$(COMPONENT_LICENSE_FILE) license='\$(COMPONENT_LICENSE)'
 
 file path=\$(USRDIR)/lib/lighttpd/\$(LIGHTTPD_MAJOR)/lib/\$(MACH64)/mod_${i}.so
 file path=\$(USRDIR)/lib/lighttpd/\$(LIGHTTPD_MAJOR)/lib/mod_${i}.so
